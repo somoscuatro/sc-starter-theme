8dd9cd4f6151b5a3754e1a72ed6a210a04c39795
Luca Pipolo <luca@somoscuatro.es>
2024-03-21 14:23:48 +0100

    feat: add filesystem helper class

---
+++ src/helpers/class-filesystem.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Filesystem helpers class.
+ *
+ * @package sc-starter-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Starter_Theme\Helpers;
+
+/**
+ * Filesystem helpers class.
+ */
+trait Filesystem {
+
+	/**
+	 * Base for asset URLs.
+	 *
+	 * @var string
+	 */
+	private static string $base_url = '';
+
+	/**
+	 * Returns the base URL of this theme.
+	 *
+	 * @return string Return theme URI.
+	 */
+	private function get_base_url(): string {
+		if ( ! self::$base_url ) {
+			self::$base_url = get_stylesheet_directory_uri();
+		}
+		return self::$base_url;
+	}
+
+	/**
+	 * The absolute filesystem base path of this plugin.
+	 *
+	 * @return string Return base path URI.
+	 */
+	private function get_base_path(): string {
+		return dirname( __DIR__, 2 );
+	}
+}
