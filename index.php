86903ed3e49a5e441ec4f52078b0b667f9081b29
Luca Pipolo <luca@somoscuatro.es>
2024-03-21 14:22:44 +0100

    feat: add timber basic templates

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-It works!
+<?php
+/**
+ * The index template file.
+ *
+ * @package sc-starter-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Starter_Theme;
+
+use Timber\Timber as TimberLibrary;
+
+$context = TimberLibrary::context();
+
+TimberLibrary::render( 'index.twig', $context );
