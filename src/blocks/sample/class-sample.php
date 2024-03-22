3207357ffee647b9e4eee36c6a4aa42afb618267
Luca Pipolo <luca@somoscuatro.es>
2024-03-22 12:19:56 +0100

    feat: add sample block

---
+++ src/blocks/sample/class-sample.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Block's main functionality methods.
+ *
+ * @package somoscuatro-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Starter_Theme\Blocks\Sample;
+
+use Somoscuatro\Starter_Theme\Blocks\Block;
+
+/**
+ * Block main functionality.
+ */
+class Sample extends Block {
+
+	/**
+	 * The prefix used for ACF Blocks.
+	 *
+	 * @var string
+	 */
+	public static $acf_block_prefix = 'block_sample';
+
+	/**
+	 * Gets the ACF Block fields.
+	 *
+	 * @return array The ACF Block fields.
+	 */
+	public function get_acf_fields(): array {
+		return array(
+			'key'      => 'group_' . static::$acf_block_prefix,
+			'title'    => __( 'Block: Sample', 'sc-starter-theme' ),
+			'fields'   => array(
+				array(
+					'key'           => 'field_' . static::$acf_block_prefix . '_text',
+					'label'         => __( 'Sample Text', 'somoscuatro-theme' ),
+					'name'          => static::$acf_block_prefix . '_text',
+					'type'          => 'text',
+					'required'      => 1,
+					'return_format' => 'string',
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param'    => 'block',
+						'operator' => '==',
+						'value'    => 'acf/sample',
+					),
+				),
+			),
+		);
+	}
+}
