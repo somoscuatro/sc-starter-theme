ea8aa152a16fcedcd2b08aabdf122001a2bf2166
Luca Pipolo <luca@somoscuatro.es>
2024-03-25 16:47:40 +0100

    refactor: move enqueue assets to asset class

--- src/class-asset.php
+++ src/class-asset.php
@@ -9,10 +9,23 @@ namespace Somoscuatro\Starter_Theme;
 
 use Somoscuatro\Starter_Theme\Attributes\Action;
 
+use Somoscuatro\Starter_Theme\Helpers\Filesystem;
+
 /**
  * Assets management class.
  */
 class Asset {
+
+	use Filesystem;
+
+	/**
+	 * Enqueues frontend theme styles and scripts.
+	 */
+	#[Action( 'wp_enqueue_scripts' )]
+	public function enqueue_assets(): void {
+		// Theme styles.
+		wp_enqueue_style( Theme::PREFIX, $this->get_base_url() . '/dist/styles/main.css', array(), $this->get_filemtime( 'styles/main.css' ) );
+	}
 
 	/**
 	 * Enqueues editor theme styles and scripts.
