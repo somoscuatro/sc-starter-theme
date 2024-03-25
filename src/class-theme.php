8616bbaa00ee83cddcfbdd6fb3c62fa3e5a695d0
Luca Pipolo <luca@somoscuatro.es>
2024-03-25 16:51:16 +0100

    feat: add page slug to body classes

--- src/class-theme.php
+++ src/class-theme.php
@@ -8,6 +8,7 @@
 namespace Somoscuatro\Starter_Theme;
 
 use Somoscuatro\Starter_Theme\Attributes\Action;
+use Somoscuatro\Starter_Theme\Attributes\Filter;
 
 use Somoscuatro\Starter_Theme\Blocks\Loader as BlocksLoader;
 use Somoscuatro\Starter_Theme\Custom_Post_Types\Loader as CustomPostTypeLoader;
@@ -42,5 +43,32 @@ class Theme {
 	public function theme_support(): void {
 		add_theme_support( 'title-tag' );
 		add_theme_support( 'post-thumbnails' );
+	}
+
+	/**
+	 * Adds page slug to body classes.
+	 *
+	 * @param array $classes The body classes.
+	 *
+	 * @return array The modified body classes.
+	 */
+	#[Filter( 'body_class' )]
+	public static function body_class( array $classes ): array {
+		if ( is_single() || ( is_page() && ! is_front_page() ) ) {
+			if ( ! in_array( basename( get_permalink() ), $classes, true ) ) {
+				$classes[] = basename( get_permalink() );
+			}
+		}
+		if ( is_page() && has_post_parent() ) {
+			global $post;
+			if ( ! in_array( basename( get_permalink( $post->post_parent ) ), $classes, true ) ) {
+				$classes[] = basename( get_permalink( $post->post_parent ) );
+			}
+		}
+		if ( is_404() ) {
+			$classes[] = '404';
+		}
+
+		return $classes;
 	}
 }
