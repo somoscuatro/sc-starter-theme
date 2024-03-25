ddc1eac15b2daa00229af4eee4c6ec7fe036162f
Luca Pipolo <luca@somoscuatro.es>
2024-03-25 11:14:18 +0100

    feat: add tailwind custom colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+import * as tailwindColorPalette from "./tailwind.colors.json";
+import * as tailwindBgColorsSafeList from "./tailwind.bg-colors-safelist.json";
+
 module.exports = {
 	content: [
 		"./src/**/*.{php,twig}",
 		"./templates/**/*.twig",
 		"./assets/**/*.js",
 	],
-	theme: {},
+	theme: {
+		extend: {
+			colors: tailwindColorPalette,
+		},
+	},
 	plugins: [],
-	safelist: [],
+	safelist: [...tailwindBgColorsSafeList.colors],
 };
