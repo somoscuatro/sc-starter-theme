2e9f8a90d17f4db9beef03a9e5dc667af1eaa5af
Luca Pipolo <luca@somoscuatro.es>
2024-03-26 12:53:00 +0100

    style: eslint and prettier issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
-import * as tailwindColorPalette from "./tailwind.colors.json";
-import * as tailwindBgColorsSafeList from "./tailwind.bg-colors-safelist.json";
+import * as tailwindColorPalette from './tailwind.colors.json';
+import * as tailwindBgColorsSafeList from './tailwind.bg-colors-safelist.json';
 
 module.exports = {
 	content: [
-		"./src/**/*.{php,twig}",
-		"./templates/**/*.twig",
-		"./assets/**/*.js",
+		'./src/**/*.{php,twig}',
+		'./templates/**/*.twig',
+		'./assets/**/*.js',
 	],
 	theme: {
 		container: {
 			center: true,
 			screens: {
-				sm: "640px",
-				md: "768px",
-				lg: "1024px",
-				xl: "1280px",
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
 			},
 			padding: {
-				DEFAULT: "1rem",
-				"2xl": "0",
+				DEFAULT: '1rem',
+				'2xl': '0',
 			},
 		},
 		extend: {
 			fontFamily: {
-				jost: ["Jost", "Helvetica Neue", "Arial", "sans-serif"],
-				syne: ["Syne", "Helvetica Neue", "Arial", "sans-serif"],
+				jost: ['Jost', 'Helvetica Neue', 'Arial', 'sans-serif'],
+				syne: ['Syne', 'Helvetica Neue', 'Arial', 'sans-serif'],
 			},
 			colors: tailwindColorPalette,
 		},
