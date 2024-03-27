0bc4266b20628cb24eebd2e7828a72898427daeb
Luca Pipolo <luca@somoscuatro.es>
2024-03-27 17:56:17 +0100

    feat: add required plugins warning

--- src/class-theme.php
+++ src/class-theme.php
@@ -46,6 +46,30 @@ class Theme {
 	}
 
 	/**
+	 * Displays missing plugins warning.
+	 */
+	#[Action( 'admin_notices' )]
+	public function display_missing_plugins_warning(): void {
+		if ( ! is_plugin_active( 'advanced-custom-fields/acf.php' ) && ! is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) :
+			?>
+		<div class="error">
+			<p>
+				<?php
+					echo wp_kses_post(
+						sprintf(
+							/* translators: %s plugin name and link to plugin vendor page. */
+							__( '%s plugin is not installed. Please install the plugin to use the Somoscuatro Starter Theme.', 'sc-starter-theme' ),
+							'<a href="https://www.advancedcustomfields.com/" target="_blank">Advanced Custom Fields (ACF)</a>'
+						)
+					);
+				?>
+			</p>
+		</div>
+			<?php
+		endif;
+	}
+
+	/**
 	 * Adds page slug to body classes.
 	 *
 	 * @param array $classes The body classes.
