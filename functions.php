e21bd3b970e1ec415fec826c1c3384f9ec7529ac
Joan López <joan@somoscuatro.es>
2024-03-29 18:08:50 +0100

    feat: register timber as a dependency

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ use Somoscuatro\Starter_Theme\CLI\CLI;
 use Somoscuatro\Starter_Theme\Attributes\Hook;
 use Somoscuatro\Starter_Theme\Dependency_Injection\Container as Dependencies;
 use Somoscuatro\Starter_Theme\Theme;
+use Somoscuatro\Starter_Theme\Timber;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
@@ -24,6 +25,7 @@ require_once __DIR__ . '/autoload.php';
 // Register dependencies.
 $dependencies = new Dependencies();
 $dependencies->add( 'Theme', fn ( $dependencies ) => new Theme( $dependencies ) );
+$dependencies->add( 'Timber', fn () => new Timber() );
 
 // Register WordPress hooks.
 ( new Hook() )->register_hooks();
