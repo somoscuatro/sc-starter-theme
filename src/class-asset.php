b437f1aefcecf89a6536b2a508f952c8d03b1d4b
Joan López <joan@somoscuatro.es>
2024-03-29 15:56:48 +0100

    refactor: use dependencies container in assets management class

--- src/class-asset.php
+++ src/class-asset.php
@@ -8,6 +8,7 @@
 namespace Somoscuatro\Starter_Theme;
 
 use Somoscuatro\Starter_Theme\Attributes\Action;
+use Somoscuatro\Starter_Theme\Dependency_Injection\Container_Interface as Dependencies;
 
 use Somoscuatro\Starter_Theme\Helpers\Filesystem;
 
@@ -19,26 +20,47 @@ class Asset {
 	use Filesystem;
 
 	/**
+	 * Dependencies container.
+	 *
+	 * @var Dependencies
+	 */
+	protected $dependencies;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param Dependencies $dependencies Dependencies container.
+	 */
+	public function __construct( Dependencies $dependencies ) {
+		$this->dependencies = $dependencies;
+	}
+
+	/**
 	 * Enqueues frontend theme styles and scripts.
 	 */
 	#[Action( 'wp_enqueue_scripts' )]
 	public function enqueue_assets(): void {
+		$theme        = $this->dependencies->get( 'Theme' );
+		$theme_prefix = $theme->get_prefix();
+
 		// Custom fonts.
-		wp_enqueue_style( Theme::PREFIX . '-fonts-preload', $this->get_base_url() . '/dist/styles/fonts.css', false, $this->get_filemtime( 'styles/fonts.css' ) );
+		wp_enqueue_style( $theme_prefix . '-fonts-preload', $this->get_base_url() . '/dist/styles/fonts.css', false, $this->get_filemtime( 'styles/fonts.css' ) );
 
 		// Theme styles.
-		wp_enqueue_style( Theme::PREFIX, $this->get_base_url() . '/dist/styles/main.css', array( Theme::PREFIX . '-fonts-preload' ), $this->get_filemtime( 'styles/main.css' ) );
+		wp_enqueue_style( $theme_prefix . '-main-styles', $this->get_base_url() . '/dist/styles/main.css', array( $theme_prefix . '-fonts-preload' ), $this->get_filemtime( 'styles/main.css' ) );
 	}
 
 	/**
 	 * Enqueues editor theme styles and scripts.
 	 */
 	#[Action( 'admin_enqueue_scripts' )]
-	public function enqueue_admin_assets() {}
+	public function enqueue_admin_assets() {
+	}
 
 	/**
 	 * Enqueues wp-login theme styles and scripts.
 	 */
 	#[Action( 'login_enqueue_scripts' )]
-	public function enqueue_login_assets() {}
+	public function enqueue_login_assets() {
+	}
 }
